c74f960a9cda902d633be9dee5f9203fe40d2b56
kjlee772 <kjlee50612@gmail.com>
2021-06-03 23:43:18 +0900

    촬영 및 앨범 이미지 불러오기

--- App.js
+++ App.js
@@ -1,164 +1,244 @@
-import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Button } from 'react-native';
-import { launchCamera, launchImageLibrary, showImagePicker } from 'react-native-image-picker';
+import React, { Fragment, Component } from 'react';
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    Image,
+    Button,
+    Dimensions,
+    TouchableOpacity
+} from 'react-native';
 
-export default class App extends Component {
+import {
+    Header,
+    LearnMoreLinks,
+    Colors,
+    DebugInstructions,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-    options = {
-        title: 'Load Photo',
-        customButtons: [
-            { name: 'button_id_1', title: 'CustomButton 1' },
-            { name: 'button_id_2', title: 'CustomButton 2' }
-        ],
-        storageOptions: {
+const options = {
+    title: 'Select Avatar',
+    customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+    storageOptions: {
         skipBackup: true,
         path: 'images',
-        },
-    };
-
-    sip = () => {
-        showImagePicker({}, (response) => {
-            console.log('Response= ',response);
-
-            setImageSource(response.uri);
-        });
-    };
-
-    sc = () => {
-        launchCamera({}, (response) => {
-            if (response.error) {
-                console.log('LaunchCamera Error: ', response.error);
-            }
-            else {
-                setImageSource(response.uri);
-            }
-        });
-    };
+    },
+};
+export default class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            filepath: {
+                data: '',
+                uri: ''
+            },
+            fileData: '',
+            fileUri: ''
+        }
+    }
     
-    scr = () => {
-        launchImageLibrary({}, (response) => {
-            if (response.error) {
-                console.log('LaunchImageLibrary Error: ', response.error);
-            }
-            else {
-                setImageSource(response.uri);
+    chooseImage = () => {
+        let options = {
+            title: 'Select Image',
+            customButtons: [
+                { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
+            ],
+            storageOptions: {
+                skipBackup: true,
+                path: 'images',
+            },
+        };
+        launchImageLibrary(options, (response) => {
+            console.log('Response = ', response['assets'][0].uri);
+            
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+                alert(response.customButton);
+            } else {
+                const source = { uri: response.uri };
+                
+                // You can also display the image using data:
+                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
+                // alert(JSON.stringify(response));
+                
+                // console.log('response', JSON.stringify(response));
+                this.setState({
+                    filePath: response,
+                    fileData: response.data,
+                    fileUri: response['assets'][0].uri
+                });
             }
         });
-    };
+    }
+    
+    launchCamera = () => {
+        let options = {
+            storageOptions: {
+                skipBackup: true,
+                path: 'images',
+            },
+        };
+        launchCamera(options, (response) => {
+            console.log('Response = ', response);
 
-    render () {
+           if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+                alert(response.customButton);
+            } else {
+                const source = { uri: response.uri };
+                console.log('response', JSON.stringify(response));
+                this.setState({
+                    filePath: response,
+                    fileData: response.data,
+                    fileUri: response['assets'][0].uri
+                });
+            }
+        });
+    }
+
+    renderFileData() {
+        if (this.state.fileData) {
+            return <Image source={{ uri: 'data:image/jpeg;base64,' + this.state.fileData }}
+            style={styles.images}
+          />
+        } else {
+            return <Image source={require('./wakeupcat.jpg')}
+            style={styles.images}
+          />
+        }
+    }
+
+    renderFileUri() {
+        if (this.state.fileUri) {
+          return <Image
+            source={{ uri: this.state.fileUri }}
+            style={styles.images}
+          />
+        } else {
+            return <Image
+            source={require('./wakeupcat.jpg')}
+            style={styles.images}
+          />
+        }
+    }
+
+    render() {
         return (
-            <View style={styles.container}>
-                <Button
-                    title="촬영"
-                    onPress={() => this.sip()}
-                />
-                <Button
-                    title="앨범"
-                    onPress={() => this.sc()}
-                />
-                <Button
-                    title="뭐고"
-                    onPress={() => this.scr()}
-                />
-                <Button
-                    title="아카이브"
+            <Fragment>
+                <StatusBar barStyle="dark-content" />
+                <SafeAreaView>
+                    <View style={styles.body}>
+                        <Text style={{textAlign:'center',fontSize:20,paddingBottom:10}} >Pick Images from Camera & Gallery</Text>
+                        <View style={styles.ImageSections}>
+                            <View>
+                                {this.renderFileData()}
+                                <Text  style={{textAlign:'center'}}>Base 64 String</Text>
+                            </View>
+                            <View>
+                                {this.renderFileUri()}
+                                <Text style={{textAlign:'center'}}>File Uri</Text>
+                            </View>
+                        </View>
 
-                />
+                        <View style={styles.btnParentSection}>
+                            <TouchableOpacity onPress={this.chooseImage} style={styles.btnSection}  >
+                                <Text style={styles.btnText}>Choose File</Text>
+                            </TouchableOpacity>
 
-            </View>
-        )
+                            <TouchableOpacity onPress={this.launchCamera} style={styles.btnSection}  >
+                                <Text style={styles.btnText}>Directly Launch Camera</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </SafeAreaView>
+            </Fragment>
+        );
     }
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'pink',
-        //alignItems: 'center',
-        //justifyContent: 'center'
-    },
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
 
-    avatar: {
-        width: '100%',
-        height: 400
-    },
-
+  body: {
+    backgroundColor: Colors.white,
+    justifyContent: 'center',
+    borderColor: 'black',
+    borderWidth: 1,
+    height: Dimensions.get('screen').height - 20,
+    width: Dimensions.get('screen').width
+  },
+  ImageSections: {
+    display: 'flex',
+    flexDirection: 'row',
+    paddingHorizontal: 8,
+    paddingVertical: 8,
+    justifyContent: 'center'
+  },
+  images: {
+    width: 150,
+    height: 150,
+    borderColor: 'black',
+    borderWidth: 1,
+    marginHorizontal: 3
+  },
+  btnParentSection: {
+    alignItems: 'center',
+    marginTop:10
+  },
+  btnSection: {
+    width: 225,
+    height: 50,
+    backgroundColor: '#DCDCDC',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 3,
+    marginBottom:10
+  },
+  btnText: {
+    textAlign: 'center',
+    color: 'gray',
+    fontSize: 14,
+    fontWeight:'bold'
+  }
 });
-// import React, { Component } from 'react';
-// import { View, Text, StyleSheet, Image, Button } from 'react-native';
-// import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 
-// export default class App extends Component {
 
-//     state = {
-//         avatar: '이미지 uri',
-//         result: '결과',
-//         test_num:0
+
+// {
+//     "assets": [
+//         {
+//             "fileName": "rn_image_picker_lib_temp_a07c8313-1f6d-4d76-8163-72c7a574af94.jpg", 
+//             "fileSize": 3933534, 
+//             "height": 3024, 
+//             "type": "image/jpeg", 
+//             "uri": "file:///data/user/0/com.test_1/cache/rn_image_picker_lib_temp_a07c8313-1f6d-4d76-8163-72c7a574af94.jpg", 
+//             "width": 4032
+//         }
+//     ]
+// }
+
+// [
+//     {
+//         "fileName": "rn_image_picker_lib_temp_f3c8d0c4-b22e-4e3c-9fbf-bded41c2bf4c.jpg", 
+//         "fileSize": 1257184, 
+//         "height": 1440, 
+//         "type": "image/jpeg", 
+//         "uri": "file:///data/user/0/com.test_1/cache/rn_image_picker_lib_temp_f3c8d0c4-b22e-4e3c-9fbf-bded41c2bf4c.jpg", 
+//         "width": 1080
 //     }
-
-//     //촬영
-//     addImage = () => {
-//         launchCamera( {}, response => {
-//             this.setState({
-//                 avatar: response.uri
-//             })
-//         })
-//         //ocr(this.state.avatar)
-//         console.log('통과');
-//     }
-
-//     //앨범선택
-//     ChoosePhoto = () => {
-//          launchImageLibrary( {}, response => {
-//                 this.setState({
-//                     avatar: response.uri
-//                 })
-//          })
-//          //ocr(this.state.avatar)
-//     }
-
-//     render () {
-//         return (
-//             <View style={styles.container}>
-
-//                 <Image
-//                     style={styles.avatar}
-//                     source={{uri: this.state.avatar}}
-//                 />
-
-//                 <Text style={{margin:8}}> {this.state.avatar}</Text>
-//                 <Text style={{margin:8}}> {this.state.result}</Text>
-//                 <Text style={{margin:8}}> {this.state.test_num}</Text>
-//                 <Button
-//                     title="촬영"
-//                     onPress={() => this.addImage()}
-//                 />
-//                 <Button
-//                     title="앨범"
-//                     onPress={() => this.ChoosePhoto()}
-//                 />
-//                 <Button
-//                     title="아카이브"
-
-//                 />
-
-//             </View>
-//         )
-//     }
-// };
-
-// const styles = StyleSheet.create({
-//     container: {
-//         flex: 1,
-//         backgroundColor: 'pink',
-//         //alignItems: 'center',
-//         //justifyContent: 'center'
-//     },
-
-//     avatar: {
-//         width: '100%',
-//         height: 400
-//     },
-
-// });
+// ]
