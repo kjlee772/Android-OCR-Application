3b1a5843180d68b83293794b54a03e71fcd114a0
kjlee772 <kjlee50612@gmail.com>
2021-08-08 09:52:19 +0900

    서버 통신 및 비전 연결

--- home_screen.js
+++ home_screen.js
@@ -5,158 +5,135 @@ import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 import ImagePicker from 'react-native-image-crop-picker';
 import * as RNFS from 'react-native-fs';
 
+const image_option = {
+    quality: 1,
+    includeBase64: true,
+}
+
 export default class App extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            filepath: {
-                data: '',
-                uri: ''
-            },
-            fileData: '',
-            fileUri: '',
-            base_64: '',
-            mime: '',
-            path: '',
-            Res: '',
+            file_uri: null,
+            file_size: null,
+            file_base64: null,
+            file_name: null,
+            file_data: null,
         }
-        // console.log("ImagePicker: " + ImagePicker);
-        // console.log("openPicker: " + ImagePicker.openPicker);
-        // console.log("openCamera: " + ImagePicker.openCamera);
     }
 
-    chooseImage = () => {
-        let options = {
-            title: 'Select Image',
-            customButtons: [
-                { name: 'customOptionKey', title: 'Choose Photo from Custom Option' },
-            ],
-            storageOptions: {
-                skipBackup: true,
-                path: 'images',
-            },
-            // includeBase64: true,
-        };
-        launchImageLibrary(options, (response) => {
+    chooseImage() {
+        console.log('choose image called');
+        launchImageLibrary(image_option, (response) => {
             if (response.didCancel) {
                 console.log('User cancelled image picker');
-            } else if (response.error) {
+            }
+            else if (response.error) {
                 console.log('ImagePicker Error: ', response.error);
-            } else if (response.customButton) {
-                console.log('User tapped custom button: ', response.customButton);
-                alert(response.customButton);
-            } else {
-                console.log("LaunchImageLibrary= ");
-                var temp = JSON.stringify(response);
-                console.log(temp);
-                const source = { uri: response.uri };
-                // You can also display the image using data:
-                // const source = { uri: 'data:image/jpeg;base64,' + response.data };
-                // alert(JSON.stringify(response));
-
-                // console.log('response', JSON.stringify(response));
+            }
+            else {
                 this.setState({
-                    filePath: response,
-                    fileData: response['assets'][0].uri,
-                    fileUri: response['assets'][0].uri
+                    file_uri: response['assets'][0].uri,
+                    file_size: response['assets'][0].fileSize,
+                    file_base64: response['assets'][0].base64,
+                    file_name: response['assets'][0].fileName,
                 });
+                console.log('uri: ' + this.state.file_uri);
+                console.log('size: ' + this.state.file_size);
+                // console.log('base64: '+this.state.file_base64);
+                console.log('filename: ' + this.state.file_name);
             }
         });
     }
     launchCamera() {
-        let options = {
-            storageOptions: {
-                skipBackup: true,
-                path: 'images',
-            },
-        };
-        launchCamera(options, (response) => {
+        console.log('launch camera called');
+        launchCamera(image_option, (response) => {
             if (response.didCancel) {
                 console.log('User cancelled image picker');
-            } else if (response.error) {
+            }
+            else if (response.error) {
                 console.log('ImagePicker Error: ', response.error);
-            } else if (response.customButton) {
-                console.log('User tapped custom button: ', response.customButton);
-                alert(response.customButton);
-            } else {
-                console.log("LaunchCamera= " + JSON.stringify(response));
-                const source = { uri: response.uri };
+            }
+            else {
                 this.setState({
-                    filePath: response,
-                    fileData: response['assets'][0].uri,
-                    fileUri: response['assets'][0].uri
+                    file_uri: response['assets'][0].uri,
+                    file_size: response['assets'][0].fileSize,
+                    file_base64: response['assets'][0].base64,
+                    file_name: response['assets'][0].fileName,
                 });
+                console.log('uri: ' + this.state.file_uri);
+                console.log('size: ' + this.state.file_size);
+                // console.log('base64: '+this.state.file_base64);
+                console.log('filename: ' + this.state.file_name);
             }
         });
     }
-
-    renderFileData() {
-        if (this.state.fileData) {
-            return <Image source={{ uri: 'data:image/jpeg;base64,' + this.state.fileData }}
-                style={styles.images}
-            />
-        }
-        else {
-            return <Image source={require('./wakeupcat.jpg')}
-                style={styles.images}
-            />
-        }
-    }
-
-    renderFileUri() {
-        if (this.state.fileUri) {
-            return <TouchableOpacity t_uri={this.state} onPress={() => this.edit_image()}>
-                <Image source={{ uri: this.state.fileUri }}
-                    style={styles.images}
-                />
-            </TouchableOpacity>
-        } else {
-            return <TouchableOpacity onPress={() => this.edit_image()}>
-                <Image source={require('./wakeupcat.jpg')}
-                    style={styles.images}
-                />
-            </TouchableOpacity>
-        }
-    }
-
-    edit_image() {
-        ImagePicker.openPicker({
-            width: 3000,
-            height: 3000,
-            cropping: true,
+    editImage() {
+        console.log('edit image called');
+        ImagePicker.openCropper({
+            path: this.state.file_uri,
+            width: 1024,
+            height: 1024,
             includeBase64: true,
             freeStyleCropEnabled: true,
         }).then(image => {
-            console.log('path: '+image.path);
             this.setState({
-                base_64: image.data,
-                mime: image.mime,
-                path: image.path,
-            })
+                file_uri: image.path,
+                file_size: image.size,
+                file_base64: image.data,
+            });
+            console.log('uri: ' + this.state.file_uri);
+            console.log('size: ' + this.state.file_size);
+            // console.log('base64: '+this.state.file_base64);
+            console.log('filename: ' + this.state.file_name);
         });
     }
 
-    move_screen(temp) {
-        ImagePicker.clean().then(() => {
-            console.log('removed all tmp images from tmp directory');
-        }).catch(e => {
-            alert(e);
-        });
-        this.props.navigation.navigate(temp);
-    }
-
-    render_img() {
-        if (this.state.base_64) {
-            console.log("tlqkf")
-            // console.log(this.state.base_64);
-            // console.log(this.state.mime)
-            // console.log(this.state.base_64)
-            return <Image style={{ width: 300, height: 300 }} source={{ uri: `data:${this.state.mime};base64,${this.state.base_64}` }} />
-            // return <Image style={{ width: 300, height: 300 }} source={{ uri: this.state.path }} />
+    render_image() {
+        if (this.state.file_uri) {
+            this.send_image();
+            return (
+                <TouchableOpacity style={{ width: '100%', height: '100%' }} onPress={() => this.editImage()} >
+                    <Image source={{ uri: this.state.file_uri }} style={styles.images} />
+                </TouchableOpacity>
+            );
+        }
+        else {
+            return (
+                <TouchableOpacity style={{ width: '100%', height: '100%' }}>
+                    <Image source={require('./wakeupcat.jpg')} style={styles.images} />
+                </TouchableOpacity>
+            );
         }
     }
 
-    ocr = () => {
+    move_screen(temp) {
+        this.props.navigation.navigate(temp, { file_uri: this.state.file_uri, file_data: this.state.file_data});
+    }
+
+    send_image() {
+        console.log('send image called');
+        fetch('http://221.158.52.168:3001/sendImage', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json',
+            },
+            body: JSON.stringify({
+                name: this.state.file_name,
+                data: this.state.file_base64,
+            }),
+        })
+            .then(res => res.json())
+            .then(res => {
+                console.log(res.message);
+            })
+            .catch(err => {
+                console.log('send image 문제: ' + err.message, err.code);
+            });
+    }
+    ocr() {
+        console.log('ocr called');
         fetch('http://221.158.52.168:3001/ocr', {
             method: 'POST',
             headers: {
@@ -164,41 +141,41 @@ export default class App extends React.Component {
                 'Accept': 'application/json',
             },
             body: JSON.stringify({
-                data: this.state.base_64,
-                name: 'temp1',
+                name: this.state.file_name,
             }),
         })
             .then(res => res.json())
-            .then(data => {
-                console.log("Here: " + data.Res)
+            .then(res => {
+                console.log(res.Res);
+                this.setState({
+                    file_data: res.Res,
+                });
             })
             .catch(err => {
-                console.log(err.message, err.code);
+                console.log('Ocr 문제: ' + err.message, err.code);
             });
+    }
+
+    test(){
+        console.log('setState 테스트 called');
+        this.setState({
+            file_size: null,
+        });
     }
 
     render() {
         return (
             <View style={styles.container}>
-                <View style={styles.image_section}>
-                    {this.renderFileUri()}
-                    {this.render_img()}
+                <View style={styles.view_image}>
+                    {this.render_image()}
                 </View>
-                <View style={styles.button_section}>
-                    <View style={{ flexDirection: 'row' }}>
-                        <TouchableOpacity onPress={this.launchCamera} style={styles.btn}  >
-                            <Text style={styles.btn_text}>사진 촬영하기</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={this.chooseImage} style={styles.btn}  >
-                            <Text style={styles.btn_text}>사진 불러오기</Text>
-                        </TouchableOpacity>
+                <View style={styles.view_menu}>
+                    <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', marginBottom: 10 }}>
+                        <TouchableOpacity style={[styles.touch_btn, { marginRight: 10 }]} onPress={() => this.launchCamera()} ><Text style={styles.text_btn}>사진 촬영하기</Text></TouchableOpacity>
+                        <TouchableOpacity style={styles.touch_btn} onPress={() => this.chooseImage()}><Text style={styles.text_btn}>사진 불러오기</Text></TouchableOpacity>
                     </View>
-                    <TouchableOpacity style={styles.btn} onPress={() => this.ocr()}  >
-                        <Text style={styles.btn_text}>텍스트 추출하기</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.btn} onPress={() => this.move_screen('Storage')}  >
-                        <Text style={styles.btn_text}>ARCHIVE</Text>
-                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.touch_btn, { marginBottom: 10, width: 200 }]} onPress={() => this.ocr()} ><Text style={styles.text_btn}>텍스트 추출하기</Text></TouchableOpacity>
+                    <TouchableOpacity style={[styles.touch_btn, { width: 120 }]} onPress={()=>this.test()} ><Text style={styles.text_btn}>저장소</Text></TouchableOpacity>
                 </View>
             </View>
         );
@@ -207,56 +184,41 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        borderColor: 'black',
-        borderWidth: 1,
-        height: Dimensions.get('screen').height - 20,
-        width: Dimensions.get('screen').width,
-    },
-
-    scrollView: {
-        backgroundColor: 'white',
-    },
-
-    image_section: {
-        display: 'flex',
-        paddingHorizontal: 8,
-        paddingVertical: 8,
-        justifyContent: 'center',
-        flex: 3,
-        backgroundColor: '#CCFFFF',
-    },
-
-    images: {
-        height: '100%',
-        width: '100%',
-        resizeMode: 'contain',
-        backgroundColor: 'black',
-    },
-
-    button_section: {
-        alignItems: 'center',
-        marginTop: 10,
         flex: 1,
-        justifyContent: 'flex-end',
-        marginBottom: 20,
-    },
-
-    btn: {
-        width: 150,
-        height: 50,
-        backgroundColor: '#DCDCDC',
         alignItems: 'center',
         justifyContent: 'center',
-        borderRadius: 3,
-        marginBottom: 10
     },
-
-    btn_text: {
-        textAlign: 'center',
-        color: 'gray',
-        fontSize: 20,
-        fontWeight: 'bold'
-    }
+    view_image: {
+        flex: 3,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+    },
+    view_menu: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100%',
+    },
+    images: {
+        backgroundColor: 'black',
+        resizeMode: 'contain',
+        width: '100%',
+        height: '100%',
+    },
+    text_btn: {
+        fontSize: 27,
+        fontWeight: 'bold',
+    },
+    touch_btn: {
+        backgroundColor: 'gray',
+        width: 170,
+        height: 50,
+        borderRadius: 15,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
 });
--- ocr_screen.js
+++ ocr_screen.js
@@ -1,18 +1,26 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 
 export default class ocr_screen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            file_uri: this.props.route.params.file_uri,
+            file_data: this.props.route.params.file_data,
         }
+        console.log('받은 거: '+this.state.file_data);
     }
 
     render() {
         return (
-            <View>
-                <Text>OCR</Text>
+            <View style={{flex:1,alignItems:'center',justifyContent:'center'}}>
+                <View style={{flex:1,width:'100%',height:'100%',alignItems:'center',justifyContent:'center',backgroundColor:'black'}}>
+                    <Image style={{height:'100%',width:'100%',resizeMode:'contain'}} source={{uri:this.state.file_uri}} />
+                    
+                </View>
+                <View style={{flex:3}}>
+
+                </View>
             </View>
     );
     }
