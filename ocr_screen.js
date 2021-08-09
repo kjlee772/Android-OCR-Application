130687403a0f3e3fcfff9034310549626f0fcdd9
kjlee772 <kjlee50612@gmail.com>
2021-08-09 20:42:21 +0900

    ocr페이지 구현 중

--- ocr_screen.js
+++ ocr_screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, Image, TextInput, ScrollView } from 'react-native';
+import { View, Text, Image, TextInput, ScrollView,Dimensions } from 'react-native';
 
 export default class ocr_screen extends React.Component {
     constructor(props) {
@@ -13,22 +13,22 @@ export default class ocr_screen extends React.Component {
 
     render() {
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+            <View style={{ flex: 1,backgroundColor:'gray' }}>
                 <ScrollView>
-                    <View style={{ flex: 1, width: '100%', height: '100%', alignItems: 'center', justifyContent: 'center', backgroundColor: 'black' }}>
-                        <Image style={{ height: '100%', width: '100%', resizeMode: 'contain' }} source={{ uri: this.state.file_uri }} />
+                    <View style={{ flex: 1, backgroundColor: 'black',alignItems: 'center',marginBottom: 5, justifyContent: 'center' }}>
+                        <Image style={{width:Dimensions.get('window').width, height:Dimensions.get('window').height/4, resizeMode: 'contain' }} source={{ uri: this.state.file_uri }} />
                     </View>
-                    <View style={{ flex: 3, alignItems:'center',justifyContent:'center' }}>
-                        <View style={{width:'80%'}}>
-                            <TextInput style={{ backgroundColor: 'pink', color: 'black', marginBottom:10, textAlign:'center', fontSize:22, fontWeight:'bold' }} 
-                            placeholder={'주제를 입력해주세요.'} 
-                            placeholderTextColor={'black'} 
-                            onChangeText={(ch)=>{this.setState({subject:ch})}} />
+                    <View style={{ flex: 3, alignItems: 'center', justifyContent: 'center' }}>
+                        <View style={{ width: '80%' }}>
+                            <TextInput style={{ backgroundColor: 'pink', color: 'black', marginBottom: 5, textAlign: 'center', fontSize: 22, fontWeight: 'bold', borderRadius:5 }}
+                                placeholder={'주제를 입력해주세요.'}
+                                placeholderTextColor={'black'}
+                                onChangeText={(ch) => { this.setState({ subject: ch }) }} />
                         </View>
-                        <View>
-                            <TextInput style={{ backgroundColor: 'yellow', color: 'black',borderWidth:1 }} 
-                            value={this.state.file_data} placeholderTextColor={'black'} multiline={true} 
-                            onChangeText={(ch)=>{this.setState({file_data:ch})}}  />
+                        <View style={{ width: '99%' }}>
+                            <TextInput style={{backgroundColor:'white', color: 'black', borderWidth: 1, borderRadius:5 }}
+                                value={this.state.file_data} placeholderTextColor={'black'} multiline={true}
+                                onChangeText={(ch) => { this.setState({ file_data: ch }) }} />
                         </View>
                     </View>
                 </ScrollView>
