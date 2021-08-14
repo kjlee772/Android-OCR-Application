24a06440e353c368315b4fd4aa8a2ad391142cdc
kjlee772 <kjlee50612@gmail.com>
2021-08-15 00:07:03 +0900

    토큰 이상

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// 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
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
